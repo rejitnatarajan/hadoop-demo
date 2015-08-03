f5cdcdbec69299152c3fa20b6efbfed2fc98999d
Rejit Natarajan <rejit.natarajan@gmail.com>
2015-08-03 08:03:02 +0530

    initial commit

---
+++ src/main/java/com/rejit/hadoop/MapReduce/WordCountReducer.java
@@ -0,0 +1,26 @@
+
+package com.rejit.hadoop.MapReduce;
+
+/**
+ *
+ * @author rejit
+ */
+import java.io.IOException;
+ 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+ 
+public class WordCountReducer extends
+        Reducer<Text, IntWritable, Text, IntWritable> {
+ 
+    @Override
+    public void reduce(Text text, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        context.write(text, new IntWritable(sum));
+    }
+}
